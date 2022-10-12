0637b82f09f7ea4ba6ac71df754c087329c2df42
Patrick Willnow <patrick.willnow@th-luebeck.de>
2022-10-12 10:28:02 +0200

    WIP - persist

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 
-function RESTButton() {
+function ClockButton() {
     const [buttonText, updateButtonText] = useState('Press Button to get Datetime');
 
-    function handleClick() {
+    function getClock() {
         fetch('http://localhost:3080/clock', {crossDomain: true})
             .then(response => response.json())
             .then(
@@ -15,7 +15,26 @@ function RESTButton() {
     }
 
     return (
-        <button onClick={handleClick}>
+        <button onClick={getClock}>
+            {buttonText}
+        </button>
+    );
+}
+
+function DBQueryButton() {
+    const [buttonText, updateButtonText] = useState('Press Button to get a execute query');
+
+    function executeQuery() {
+        fetch('http://localhost:3080/clock', {crossDomain: true})
+            .then(response => response.json())
+            .then(
+                data => updateButtonText("Most recent date-time from rest endpoint: " + data.someDateTime)
+            )
+            .catch(reason => console.log(reason));
+    }
+
+    return (
+        <button onClick={executeQuery}>
             {buttonText}
         </button>
     );
@@ -29,7 +48,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <RESTButton/>
+        <ClockButton/>
+        <br/>
+          <label for="queryText">Input </label>
+          <input id="queryText" type="text"/>
+          <DBQueryButton/>
         {/*<a
           className="App-link"
           href="https://reactjs.org"
