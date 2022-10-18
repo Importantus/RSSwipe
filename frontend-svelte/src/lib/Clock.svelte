0f3102ff1af0e62533862e9d42c280abf3df6b82
Kjell <kjell.treder@stud.th-luebeck.de>
2022-10-18 09:58:33 +0200

    add svelte template

---
+++ frontend-svelte/src/lib/Clock.svelte
@@ -0,0 +1,17 @@
+<script>
+  let text = "Press Button to get Datetime";
+
+  function getClock() {
+    fetch("http://localhost:3080/clock", { crossDomain: true })
+      .then((response) => response.json())
+      .then((data) => {
+        text = "Most recent date-time from rest endpoint: " + data.someDateTime;
+      })
+      .catch((reason) => console.log(reason));
+  }
+</script>
+
+<div>
+  <button on:click={getClock}> Refresh Clock </button>
+  <div>{text}</div>
+</div>
---
+++ frontend-svelte/src/lib/DBQuery.svelte
@@ -0,0 +1,18 @@
+<script>
+  let queryResult = "Press Button to execute predefined query";
+
+  function executeQuery() {
+    fetch("http://localhost:3080/database/predefined", { crossDomain: true })
+      .then((response) => response.json())
+      .then((data) => {
+        queryResult =
+          "Most recent query result from rest endpoint: " + data.queryResponse;
+      })
+      .catch((reason) => console.log(reason));
+  }
+</script>
+
+<div>
+  <button on:click={executeQuery}> Predefined Query Button </button>
+  <div>{JSON.stringify(queryResult)}</div>
+</div>
