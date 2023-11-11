b1a439b920fed1af703158ec644e5fa151319a95
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-11 12:14:34 +0100

    :card_file_box: Create new initial migration

--- backend/prisma/migrations/20231111105800_init/migration.sql
+++ backend/prisma/migrations/20231111105800_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE `User` (
 
 -- CreateTable
 CREATE TABLE `Feed` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `link` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
@@ -23,38 +23,40 @@ CREATE TABLE `Feed` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `UserHasFeed` (
-    `userId` INTEGER NOT NULL,
-    `feedId` INTEGER NOT NULL,
+CREATE TABLE `FeedList` (
+    `userId` VARCHAR(191) NOT NULL,
+    `feedId` VARCHAR(191) NOT NULL,
+    `openInApp` BOOLEAN NOT NULL DEFAULT true,
 
     PRIMARY KEY (`userId`, `feedId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Article` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `link` VARCHAR(191) NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `feedId` INTEGER NOT NULL,
+    `feedId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `settings` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Settings` (
+    `id` VARCHAR(191) NOT NULL,
     `expTimeRead` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `expTimeUnread` DATETIME(3) NOT NULL,
-    `userId` INTEGER NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `ArticleHasUser` (
-    `articleId` INTEGER NOT NULL,
-    `userId` INTEGER NOT NULL,
+CREATE TABLE `ArticleList` (
+    `articleId` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
     `saved` BOOLEAN NULL,
     `read` BOOLEAN NULL,
     `dateSaved` DATETIME(3) NULL,
@@ -64,19 +66,19 @@ CREATE TABLE `ArticleHasUser` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `UserHasFeed` ADD CONSTRAINT `UserHasFeed_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `FeedList` ADD CONSTRAINT `FeedList_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `UserHasFeed` ADD CONSTRAINT `UserHasFeed_feedId_fkey` FOREIGN KEY (`feedId`) REFERENCES `Feed`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `FeedList` ADD CONSTRAINT `FeedList_feedId_fkey` FOREIGN KEY (`feedId`) REFERENCES `Feed`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `Article` ADD CONSTRAINT `Article_feedId_fkey` FOREIGN KEY (`feedId`) REFERENCES `Feed`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `settings` ADD CONSTRAINT `settings_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Settings` ADD CONSTRAINT `Settings_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ArticleHasUser` ADD CONSTRAINT `ArticleHasUser_articleId_fkey` FOREIGN KEY (`articleId`) REFERENCES `Article`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ArticleList` ADD CONSTRAINT `ArticleList_articleId_fkey` FOREIGN KEY (`articleId`) REFERENCES `Article`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `ArticleHasUser` ADD CONSTRAINT `ArticleHasUser_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `ArticleList` ADD CONSTRAINT `ArticleList_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
