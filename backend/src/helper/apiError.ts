efbd856eaff5fcd221c75120e7c680562ede6c0c
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-11 21:44:07 +0100

    :goal_net: Add apiError class

---
+++ backend/src/helper/apiError.ts
@@ -0,0 +1,30 @@
+export default class APIError extends Error {
+    status: number;
+
+    private constructor(message: string, status: number) {
+        super(message);
+        this.status = status;
+    }
+
+    toJSON() {
+        return {
+            message: this.message
+        }
+    }
+
+    static notAuthorized() {
+        return new APIError("Not authorized", 401);
+    }
+
+    static forbidden() {
+        return new APIError("Forbidden", 403);
+    }
+
+    static notFound() {
+        return new APIError("Not found", 404);
+    }
+
+    static badRequest(message: any) {
+        return new APIError("Bad request: " + message, 400);
+    }
+}
