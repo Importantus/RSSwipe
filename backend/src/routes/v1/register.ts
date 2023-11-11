c234bb130ad16e1f6ce9b08425d3d91a43784ff7
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-11 21:51:00 +0100

    :sparkles: Allow registering of users

--- backend/src/routes/v1/register.ts
+++ backend/src/routes/v1/register.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import { loginUser, registerUser } from "../../models/user";
+import APIError from "../../helper/apiError";
+import h from "../../helper/errorHelper";
 
 const router = express.Router();
 
-router.get("/", (_, res) => {
-    res.json({
-        message: "Hello, world!"
+router.post("/", h(async (req, res) => {
+    // Check if request body is valid
+    if (!req.body.name || !req.body.email || !req.body.password) {
+        throw APIError.badRequest("Invalid request body");
+    }
+
+    // Register user
+    const user = await registerUser(req.body.name, req.body.email, req.body.password)
+
+    res.status(201).json({
+        token: await loginUser(req.body.email, req.body.password),
+        user
     });
-});
+
+}))
 
 export default router;
