6a03669e17f8d1a5f665b4d0cb465f569e8565f3
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-11-15 05:17:58 +0100

    🔧 Added own theme styling to tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'title': ['Merriweather'],
+      'text': ['Merriweather Sans'],
+      'text-small': ['Inter'],
+      'Merriweather Sans': ['Merriweather Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'primary': '#CF6A31',
+        'secondary': '#282828',
+        'background': '#0A0A0A',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
   },
   plugins: [],
 }
