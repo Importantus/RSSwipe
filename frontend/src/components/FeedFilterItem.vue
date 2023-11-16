275e49c1cfd244f29b418238518ea6cb73af8ca9
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-11-16 04:15:26 +0100

    ✨ Added auto trunctuate to feed-filters

--- frontend/src/components/FeedFilterItem.vue
+++ frontend/src/components/FeedFilterItem.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { VueElement, ref, getCurrentInstance } from 'vue';
 defineProps<{
     favicon: string
     title: string
@@ -11,12 +10,12 @@ function selectFeedItem() {
 </script>
 
 <template>
-    <button class="flex flex-row rounded-xl p-2 px-3 bg-secondary mx-1" @click="selectFeedItem">
+    <button class="flex flex-row rounded-xl p-2 px-3 bg-secondary-900" @click="selectFeedItem">
         <div class=" align-middle w-4 h-4">
             <img :src="favicon" alt="favicon" />
         </div>
         <div class="align-middle pl-3">
-            <p class="font-text-small text-xs text-opacity-60">{{ title }}</p>
+            <p class="w-10 font-text-detail text-xs text-opacity-60 truncate ...">{{ title }}</p>
         </div>
     </button>
 </template>
