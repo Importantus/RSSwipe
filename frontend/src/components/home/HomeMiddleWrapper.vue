2b8a6bdef15e97b93b54299c0a89eec54a58c2e1
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-11-16 04:17:45 +0100

    📱 Added card stacking to ArticleCards

--- frontend/src/components/home/HomeMiddleWrapper.vue
+++ frontend/src/components/home/HomeMiddleWrapper.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-import ArticleInfoElement from '@/components/ArticleInfoElement.vue';
+import ArticleCard from '../ArticleCard.vue';
 </script>
 
 <template>
-    <div class="mt-5 mb-5 px-5">
-        <div
-            class=" pt-72 drop-shadow-lg rounded-xl bg-center bg-cover bg-[url('https://www.buchreport.de/wp-content/uploads/2020/11/Papst-Franziskus_550x415.jpg')]">
-            <ArticleInfoElement class="rounded-xl" />
+    <div class="grid">
+        <div class="col-start-1 row-start-1">
+            <ArticleCard
+                url="https://images.tagesschau.de/image/5a2557eb-50d5-4161-b8d0-c976bade922a/AAABh_9ZeCk/AAABibBx3ic/20x9-960/symbol-vereinte-nationen-100.webp" />
+        </div>
+        <div class="col-start-1 row-start-1 mt-3">
+            <ArticleCard
+                url="https://images.tagesschau.de/image/36e09173-873a-44e7-8422-c8a7621ee13a/AAABi9YFCHM/AAABibBx3ic/20x9-960/biden-pk-san-francisco-100.webp" />
+        </div>
+        <div class="col-start-1 row-start-1 mt-6">
+            <ArticleCard url="https://www.buchreport.de/wp-content/uploads/2020/11/Papst-Franziskus_550x415.jpg" />
         </div>
     </div>
 </template>
