12b542015423b0877b8cb40b1eea4d0adf8160d8
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-18 12:35:23 +0100

    🦺 Validate userregisterinput

--- backend/src/routes/v1/register.ts
+++ backend/src/routes/v1/register.ts
@@ -2,13 +2,17 @@ import express from "express";
 import { loginUser, registerUser } from "../../models/user";
 import APIError from "../../helper/apiError";
 import h from "../../helper/errorHelper";
+import { assert } from "superstruct";
+import { UserRegisterInput } from "../../validators/user";
 
 const router = express.Router();
 
 router.post("/", h(async (req, res) => {
     // Check if request body is valid
-    if (!req.body.name || !req.body.email || !req.body.password) {
-        throw APIError.badRequest("Invalid request body");
+    try {
+        assert(req.body, UserRegisterInput)
+    } catch (err: any) {
+        throw APIError.badRequest(err.message)
     }
 
     // Register user
