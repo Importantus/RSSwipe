fc825e426c8537827c89435304b03cf28e9e4cd0
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-18 12:32:03 +0100

    💩🔧 Hardcode secrets

--- backend/src/helper/environment.ts
+++ backend/src/helper/environment.ts
@@ -1,20 +1,22 @@
-function constructEnv(name: string): string {
+function constructEnv(name: string, mandatory: boolean = true): string {
     const env = process.env[name];
-    if (!env) {
+    if (!env && mandatory) {
         throw new Error(`Environment variable ${name} not found`);
     }
-    return env
+    return env || "";
 }
 
 export const environment = {
-    jwtSecret: constructEnv("JWT_SECRET"),
-    jwtExpiration: constructEnv("JWT_EXPIRATION"),
+    // THIS IS NOT BEST PRACTICE!!! We only define the sercets here, 
+    // because we don't have access to the secret management in deployment
+    jwtSecret: "supersecret",
+    jwtExpiration: "30d",
     dbHost: constructEnv("DB_HOST"),
     dbPort: constructEnv("DB_PORT"),
     dbDatabase: constructEnv("DB_DATABASE"),
     dbUser: constructEnv("DB_USER"),
     dbPassword: constructEnv("DB_PASSWORD"),
-    status: constructEnv("STATUS"),
+    status: constructEnv("STATUS", false),
     backendPort: constructEnv("BACKEND_PORT"),
     backendURL: constructEnv("BACKEND_URL"),
     viteBackendURL: constructEnv("VITE_BACKEND_URL"),
