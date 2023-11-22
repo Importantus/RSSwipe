3d8ee871feec42f7f0374e8d69333af765b4de65
yamen Aljasem <mohammad.aljasem@stud.th-luebeck.de>
2023-11-22 16:05:45 +0100

    🌱 added get and put functions to settings

--- backend/src/routes/v1/settings.ts
+++ backend/src/routes/v1/settings.ts
@@ -1,11 +1,35 @@
 import express from "express";
+import h from "../../helper/errorHelper";
+import { getSettings, updateSettings } from "../../models/settings";
+import { assert } from "superstruct";
+import { SettingsUpdateInput } from "../../validators/settings";
+import APIError from "../../helper/apiError";
 
 const router = express.Router();
 
-router.get("/", (_, res) => {
-    res.json({
-        message: "Hello, world!"
-    });
-});
+router.get("/", h(async (_, res) => {
+    const id = res.locals.id;
+
+    res.status(200).json(
+        await getSettings(id)
+    )
+}));
+
+router.put("/", h(async (req, res) => {
+    const id = res.locals.id;
+
+    // Check if request body is valid
+    try {
+        assert(req.body, SettingsUpdateInput)
+    } catch (err: any) {
+        throw APIError.badRequest(err.message)
+    }
+
+    await updateSettings(id, req.body);
+
+    res.status(200).json(
+        await getSettings(id)
+    )
+}));
 
 export default router;
