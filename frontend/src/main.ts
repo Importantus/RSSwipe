60c43d1009c1a674825990e63297002a30ac1e98
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-24 21:59:10 +0100

    :children_crossing: Update router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,13 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
 import router from './router'
+import store from "./stores/index";
+import App from './App.vue'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import UserDataView from '@/views/UserDataView.vue'
+import { useAuthStore } from '@/stores/auth'
+import pinia from '@/stores/index'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +14,39 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: RegisterView
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: UserDataView
+    }
   ]
+})
+
+const authStore = useAuthStore(pinia)
+
+router.beforeEach((to, from, next) => {
+  // Check if the user is logged in
+  if (to.name !== 'Login' && to.name !== 'Register' && !authStore.isLoggedIn) {
+    next({ name: 'Login' })
+  } else {
+    next()
+  }
+})
+
+router.afterEach((to, from) => {
+  const toDepth = to.path.split('/').length
+  const fromDepth = from.path.split('/').length
+  to.meta.transition = toDepth < fromDepth ? 'slide-right' : 'slide-left'
 })
 
 export default router
