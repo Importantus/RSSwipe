c1d73b4672162becdfcf37dc5225a355d048da13
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-11-25 04:11:51 +0100

    💄 replaced with tertiary color

--- frontend/src/components/DiscardArticleButton.vue
+++ frontend/src/components/DiscardArticleButton.vue
@@ -6,7 +6,7 @@ import { X } from 'lucide-vue-next';
 </script>
 
 <template>
-    <button class=" bg-[#D9D9D9] rounded-full" @click="DiscardArticle">
+    <button class=" bg-tertiary-200 rounded-full" @click="DiscardArticle">
         <X size="24" class=" text-primary-600 m-3" />
     </button>
 </template>
