ccb39c170f4c7aac859dd4a0c682733dc451a430
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-27 17:55:41 +0100

    :lipstick: Highlight input icon when typing

--- frontend/src/components/TextInputIcon.vue
+++ frontend/src/components/TextInputIcon.vue
@@ -30,7 +30,8 @@ const updateValue = (event: any) => {
         <input :type="type" :value="modelValue" @input="updateValue" :placeholder="placeholder" :required="required"
             class="w-full h-14 bg-neutral-700 bg-opacity-60 rounded-lg border border-neutral-700 backdrop-blur-sm pl-12 placeholder:text-neutral-600" />
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <props.icon :size="24" color="rgb(82 82 82 / var(--tw-text-opacity)" />
+            <props.icon :size="24" :class="{ 'stroke-white': modelValue, 'stroke-neutral-600': !modelValue }"
+                class="transition-all" />
         </div>
     </div>
 </template>
