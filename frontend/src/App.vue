2d3d07fa1e89af860caf4cb02217e8aaf997536f
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-27 17:54:17 +0100

    :lipstick: Use bigger max width

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <div class="max-w-md m-auto overflow-hidden h-screen relative">
+  <div class="max-w-lg m-auto overflow-hidden h-screen relative">
     <router-view v-slot="{ Component, route }">
       <transition :name="(route.meta.transition as string)">
         <component class="absolute w-full h-screen bg-background-950" :is="Component" />
