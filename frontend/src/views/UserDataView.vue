987e7e118920efb6dad0e42822c1006d45f8eac4
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-27 17:16:09 +0100

    :sparkles: Add TitleNavigationBar

--- frontend/src/views/UserDataView.vue
+++ frontend/src/views/UserDataView.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TitleNavigationBar from '@/components/TitleNavigationBar.vue';
 import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
 
@@ -11,6 +12,9 @@ function handleLogout() {
 </script>
 
 <template>
-    <button @click="handleLogout"
-        class="w-full h-14 bg-amber-600 rounded-lg mt-10 hover:bg-amber-700 transition">Logout</button>
+    <div class="px-5">
+        <TitleNavigationBar title="Account" backNavigationPath="/" />
+        <button @click="handleLogout"
+            class="w-full h-14 bg-amber-600 rounded-lg mt-10 hover:bg-amber-700 transition">Logout</button>
+    </div>
 </template>
