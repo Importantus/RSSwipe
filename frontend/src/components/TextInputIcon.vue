e586f228ad08c451e8aae86f4bd28041efd5d94f
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-27 17:15:41 +0100

    :wheelchair: Improve contrast for placeholder text

--- frontend/src/components/TextInputIcon.vue
+++ frontend/src/components/TextInputIcon.vue
@@ -28,9 +28,9 @@ const updateValue = (event: any) => {
 <template>
     <div class="relative">
         <input :type="type" :value="modelValue" @input="updateValue" :placeholder="placeholder" :required="required"
-            class="w-full h-14 bg-neutral-700 bg-opacity-60 rounded-lg border border-neutral-700 backdrop-blur-sm pl-12 placeholder:text-neutral-700" />
+            class="w-full h-14 bg-neutral-700 bg-opacity-60 rounded-lg border border-neutral-700 backdrop-blur-sm pl-12 placeholder:text-neutral-600" />
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-            <props.icon :size="24" color="rgb(64 64 64 / var(--tw-text-opacity)" />
+            <props.icon :size="24" color="rgb(82 82 82 / var(--tw-text-opacity)" />
         </div>
     </div>
 </template>
