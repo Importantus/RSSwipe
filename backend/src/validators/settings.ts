0bf19de8ffce58e75cecd0a206d69af161f7fc35
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-11-28 16:13:35 +0100

    :label: Update time type

--- backend/src/validators/settings.ts
+++ backend/src/validators/settings.ts
@@ -1,15 +1,12 @@
 import * as s from 'superstruct';
 
 
-const timestamp = s.refine(s.number(), 'timestamp', value => {
-    
-    return Number.isInteger(value) && value > 0;
-});
+const time = s.number();
 
 
 export const SettingsUpdateInput = s.object({
-    expTimeRead: s.optional(timestamp),
-    expTimeUnread: s.optional(timestamp),
+    expTimeRead: s.optional(time),
+    expTimeUnread: s.optional(time),
 });
 
 export type SettingsUpdateInputType = s.Infer<typeof SettingsUpdateInput>;
