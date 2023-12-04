0ae6e3a465640be4c6cd3a949602e557f7962bb0
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-05 00:44:01 +0100

    🧱 Updated starred route

--- backend/src/routes/v1/starred.ts
+++ backend/src/routes/v1/starred.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import h from "../../helper/errorHelper";
+import { getStarredArticles, updateStarredArticle } from "../../models/starred";
+import { getArticle } from "../../models/articles"
+import APIError from "../../helper/apiError";
 
 const router = express.Router();
 
-router.get("/", (_, res) => {
-    res.json({
-        message: "Hello, world!"
-    });
-});
+router.get("/", h(async (_, res) => {
+    const id = res.locals.userId;
+    res.status(200).json(
+        await getStarredArticles(id)
+    );
+}))
+
+router.put("/:article", h(async (req, res) => {
+    const id = res.locals.userId;
+    await updateStarredArticle(req.body.articleId, id, req.body.starred)
+    res.status(200).json(
+        await getArticle(id, req.body.articleId)
+    );
+}))
 
 export default router;
