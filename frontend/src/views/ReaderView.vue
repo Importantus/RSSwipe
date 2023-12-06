e5db28d637537fca94e25bc990bfebdfd1ff9eb3
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-06 18:58:25 +0100

    ✨ Added new View for ArticleReader

---
+++ frontend/src/views/ReaderView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TitleNavigationBar from '@/components/TitleNavigationBar.vue';
+import ArticleReaderElement from '@/components/ArticleReaderElement.vue';
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const articleId = route.params.id.toString();
+
+
+
+
+</script>
+
+<template>
+    <div class="px-5">
+        <TitleNavigationBar title="" backNavigationPath="/" />
+        <Suspense>
+            <ArticleReaderElement :articleId="articleId" />
+        </Suspense>
+    </div>
+</template>
