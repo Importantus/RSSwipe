b6b3443fec645fc42b334a1c5b8d785345bf7e15
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-08 21:56:02 +0100

    🍱 Added StarArticleButton component

---
+++ frontend/src/components/StarArticleButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Star } from 'lucide-vue-next';
+import { ref } from 'vue';
+
+let starred = ref(false);
+
+function toggleStar() {
+    console.log("toggleStar");
+    starred.value = !starred.value;
+}
+
+</script>
+
+<template>
+    <button class="rounded-full" @click="toggleStar">
+        <Star size="28" class=" text-white m-3" :class="{ 'fill-white': starred || false }" />
+    </button>
+</template>
