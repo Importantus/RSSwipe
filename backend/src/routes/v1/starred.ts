c8ddd024fad690e79218f16b802a13c9ea7a20f7
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-09 04:15:33 +0100

    🥅 Added UserInput Error Catching

--- backend/src/routes/v1/starred.ts
+++ backend/src/routes/v1/starred.ts
@@ -3,6 +3,8 @@ import h from "../../helper/errorHelper";
 import { getStarredArticles, updateStarredArticle } from "../../models/starred";
 import { getArticle } from "../../models/articles"
 import APIError from "../../helper/apiError";
+import { uuid } from "../../validators/uuids";
+import { assert } from "superstruct";
 
 const router = express.Router();
 
@@ -13,11 +15,31 @@ router.get("/", h(async (_, res) => {
     );
 }))
 
-router.put("/:article", h(async (req, res) => {
+router.post("/:article", h(async (req, res) => {
     const id = res.locals.userId;
-    await updateStarredArticle(req.body.articleId, id, req.body.starred)
+    const articleId = req.body.id;
+    try {
+        assert(articleId, uuid)
+    } catch (err: any) {
+        throw APIError.badRequest("Invalid id")
+    }
+    await updateStarredArticle(articleId, id, true)
     res.status(200).json(
-        await getArticle(id, req.body.articleId)
+        await getArticle(id, articleId)
+    );
+}))
+
+router.delete("/:article", h(async (req, res) => {
+    const id = res.locals.userId;
+    const articleId = req.body.id;
+    try {
+        assert(articleId, uuid)
+    } catch (err: any) {
+        throw APIError.badRequest("Invalid id")
+    }
+    await updateStarredArticle(articleId, id, false)
+    res.status(200).json(
+        await getArticle(id, articleId)
     );
 }))
 
