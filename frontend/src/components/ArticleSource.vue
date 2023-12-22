311ad4f3cebeba3838ba3e6bec6db08ed9047b44
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-22 01:22:31 +0100

    💄 Only draw favicon if an image link exists

--- frontend/src/components/ArticleSource.vue
+++ frontend/src/components/ArticleSource.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-row w-fit rounded-md p-1 px-2 bg-secondary-900 bg-opacity-60 max-w-[16rem]">
-        <div class="w-4 h-4">
+        <div v-if="article.feed.faviconUrl" class="w-4 h-4 mr-2">
             <img :src="article.feed.faviconUrl" alt="favicon" />
         </div>
-        <p class="pl-2 font-text-detail font-light text-xs text-opacity-60 truncate ...">{{ article.feed.title }}</p>
+        <p class="font-text-detail font-light text-xs text-opacity-60 truncate ...">{{ article.feed.title }}</p>
     </div>
 </template>
 
