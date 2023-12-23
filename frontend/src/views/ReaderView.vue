661af6c93868b072cbb809d88dcafcfabbe4a99a
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-12-23 22:14:40 +0100

    :lipstick: Add different themes to readerview

--- frontend/src/views/ReaderView.vue
+++ frontend/src/views/ReaderView.vue
@@ -5,8 +5,9 @@ import DOMPurify from 'dompurify';
 import ArticleSource from '@/components/ArticleSource.vue';
 import ReaderFunctionElement from '@/components/ReaderFunctionElement.vue';
 import { ReaderContext, ReaderStatus, useReaderStore } from '@/stores/reader';
-import { MoveRight } from 'lucide-vue-next';
+import { MoveRight, MoveLeft } from 'lucide-vue-next';
 import { useRoute } from 'vue-router';
+import { fontSizes, fonts, colorSchemes } from '@/stores/reader';
 
 const TOLERANCE = window.innerHeight * 0.1;
 const route = useRoute();
@@ -61,8 +62,7 @@ onMounted(async () => {
 });
 
 onUpdated(() => {
-    if (!store.storedArticles[1].articleInfo.imageUrl) {
-        console.log("image url: " + store.storedArticles[1].articleInfo.imageUrl)
+    if (!store.storedArticles[1] || !store.storedArticles[1].articleInfo.imageUrl) {
         url.value = templateArr[Math.floor(Math.random() * templateArr.length)];
         console.log("image url: " + url.value)
     } else {
@@ -119,27 +119,51 @@ function calculateUIHide() {
 
 <template>
     <div>
-        <Transition name="readerbar-hide">
-            <TitleNavigationBar v-if="!hideUi" class="mx-5" :title="store.storedArticles[0].articleInfo.title"
-                :backNavigationPath="backNavigationPath" />
-        </Transition>
-        <div class="bottom-0 left-0 w-full h-1 bg-primary-600" :class="{ 'rounded-r-full': !scrollFinshed }"
-            :style="`width: ${scrollpercent}%;`">
+        <div class="fixed top-0 right-0 left-0 m-auto w-full max-w-xl">
+            <Transition name="readerbar-hide">
+                <div v-if="!hideUi"
+                    class="flex flex-row items-center justify-between py-5 px-5 z-10 bg-background-950 backdrop-blur-md bg-opacity-70">
+                    <div class="flex flex-row items-center w-full">
+                        <router-link :to="backNavigationPath">
+                            <MoveLeft size="24" class=" text-inherit" />
+                        </router-link>
+                        <h1 class="truncate ... max-width-full text-2xl font-bold text-inherit ml-2">{{
+                            store.storedArticles[0].articleInfo.title }}</h1>
+                    </div>
+                    <div class="flex flex-row items-center">
+                        <slot></slot>
+                    </div>
+                </div>
+            </Transition>
+            <div class="bottom-0 left-0 w-full h-1 bg-primary-600" :class="{ 'rounded-r-full': !scrollFinshed }"
+                :style="`width: ${scrollpercent}%;`">
+            </div>
         </div>
-        <div id="scroll" class="px-5 h-full overflow-y-scroll no-scrollbar pb-[10vh]">
-            <div v-if="articleStatus === ReaderStatus.READY">
-                <div class="pt-5 border-b-2">
-                    <img :src="store.storedArticles[0].articleInfo.imageUrl" class="pb-5">
-                    <ArticleSource class="pt-2" :article="store.storedArticles[0].articleInfo" />
-                    <div v-html="DOMPurify.sanitize(store.storedArticles[0].articleInfo.title)"
-                        class="text-3xl font-bold text-white py-5"></div>
+        <div id="scroll" class="px-5 h-full overflow-y-scroll no-scrollbar pb-[10vh] pt-20" :class="{
+            'border-[#1C1204] text-[#1C1204] bg-[#C8A777]': store.settings.colorScheme.id === colorSchemes.sepia.id,
+            'border-black text-black bg-white': store.settings.colorScheme.id === colorSchemes.light.id,
+            'border-white text-white bg-background-950': store.settings.colorScheme.id === colorSchemes.dark.id,
+        }">
+            <div v-if="articleStatus === ReaderStatus.READY" class="border-inherit">
+                <div class="pt-5 border-b-2 border-inherit">
+                    <img :src="store.storedArticles[0].articleInfo.imageUrl" class="pb-5 rounded-md">
+                    <ArticleSource class="pt-2 text-white" :article="store.storedArticles[0].articleInfo" />
+                    <div class="text-3xl font-semibold text-inherit py-5"
+                        :class="{ 'font-title': store.settings.font.id === fonts.serif.id }">{{
+                            store.storedArticles[0].articleInfo.title
+                        }}
+                    </div>
                 </div>
-                <div class="prose prose-invert"
-                    v-html="DOMPurify.sanitize(store.storedArticles[0].content?.content.content ?? '')">
+                <div class="prose" :class="{
+                            'family': store.settings.font.id === fonts.serif.id,
+                            'prose-invert': store.settings.colorScheme.id === colorSchemes.dark.id,
+                            'prose-sm': store.settings.fontSize.id === fontSizes.small.id,
+                            'prose-lg': store.settings.fontSize.id === fontSizes.large.id
+                        }" v-html="DOMPurify.sanitize(store.storedArticles[0].content?.content.content ?? '')">
                 </div>
-                <div v-if="store.storedArticles.length > 1" @click="nextArticle">
-                    <div class="border-b-2 w-full mb-5"></div>
-                    <h1 class="text-lg font-bold text-white my-2">Next Article:</h1>
+                <div v-if="store.storedArticles.length > 1" @click="nextArticle" class="border-inherit">
+                    <div class="border-b-2 border-inherit w-full mb-5"></div>
+                    <h1 class="text-lg font-bold text-inherit my-2">Next Article:</h1>
                     <div class="bg-cover bg-black bg-opacity-80 bg-blend-overlay p-5 rounded-2xl" :style="{
                         backgroundImage: 'url(' + url + ')',
                     }">
@@ -148,14 +172,12 @@ function calculateUIHide() {
                                 <h1 class="text-lg font-bold text-white w-fit line-clamp-2 overflow-ellipsis">
                                     {{
                                         store.storedArticles[1].articleInfo.title }}</h1>
-                                <ArticleSource class="mt-2" :article="store.storedArticles[1].articleInfo" />
+                                <ArticleSource class="mt-2 text-white" :article="store.storedArticles[1].articleInfo" />
                             </div>
                             <MoveRight size="38" class="w-1/3 ml-5 text-white" />
                         </div>
                     </div>
                 </div>
-
-
             </div>
             <div v-else-if="articleStatus === ReaderStatus.LOADING"
                 class="flex items-center justify-center h-full pb-[20vw]">
@@ -170,3 +192,8 @@ function calculateUIHide() {
     </div>
 </template>
 
+<style>
+.family * {
+    font-family: "Merriweather", serif;
+}
+</style>
