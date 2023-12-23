0f631fd551a6965ab79d6560f78beb74577f21a6
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-12-23 22:13:19 +0100

    Inherit tex color in TitleNavigationBar

--- frontend/src/components/TitleNavigationBar.vue
+++ frontend/src/components/TitleNavigationBar.vue
@@ -11,9 +11,9 @@ defineProps({
     <div class="flex flex-row items-center justify-between mt-5 sticky top-0 py-5 z-10">
         <div class="flex flex-row items-center w-full">
             <router-link v-if="backNavigationPath" :to="backNavigationPath">
-                <MoveLeft size="24" class=" text-white" />
+                <MoveLeft size="24" class=" text-inherit" />
             </router-link>
-            <h1 class="truncate ... max-width-full text-2xl font-bold text-white ml-2">{{ title }}</h1>
+            <h1 class="truncate ... max-width-full text-2xl font-bold text-inherit ml-2">{{ title }}</h1>
         </div>
         <div class="flex flex-row items-center">
             <slot></slot>
