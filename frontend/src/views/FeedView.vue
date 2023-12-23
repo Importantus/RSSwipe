afa79850727b590098a56115e39bb3299efac5cb
yamen <mohammad.aljasem@stud.th-luebeck.de>
2023-12-23 17:23:29 +0100

    🌱 add FeedView and FeedSettingsButton

---
+++ frontend/src/views/FeedView.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import FeedList from '@/components/list/FeedList.vue';
+import FeedSettingsButton from '@/components/FeedSettingsButton.vue';
+
+
+
+
+</script>
+
+<template>
+    
+        <FeedList />
+        <!-- <FeedSettingsButton/> -->
+
+        
+        
+    
+</template>
+
+
+
+
+<style scoped>
+.gradient {
+  background: radial-gradient(114.56% 50% at 50% 50%, #3d3d3d 0%, #101010 81.77%);
+}
+</style>
