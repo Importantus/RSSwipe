df9a554818ff5da1c0b48ef52f9199de3f649843
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-12-23 22:17:14 +0100

    :sparkles: Add reader settings

---
+++ frontend/src/components/Reader/FunctionBar/ReaderSettingsButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Settings2 } from 'lucide-vue-next'
+import { ref } from 'vue';
+import ReaderSettings from '../ReaderSettings.vue';
+
+const showSettings = ref(false);
+
+// Close settings when clicking outside of the settings
+window.addEventListener('click', (e) => {
+    if (showSettings.value) {
+        const target = e.target as HTMLElement;
+        if (!target.closest('.ReaderSettings')) {
+            showSettings.value = false;
+        }
+    }
+});
+</script>
+
+<template>
+    <div class="relative ReaderSettings">
+        <Transition name="readersettings">
+            <ReaderSettings v-if="showSettings" class="absolute top-0 left-0 -translate-x-1/3 -translate-y-[110%]" />
+        </Transition>
+        <Settings2 @click="showSettings = !showSettings" :size="28" class="m-3" />
+    </div>
+</template>
---
+++ frontend/src/components/Reader/ReaderSettings.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { colorSchemes, fonts, fontSizes } from '@/stores/reader';
+import ReaderColorSetting from './ReaderColorSetting.vue';
+import ReaderFontSetting from './ReaderFontSetting.vue';
+import ReaderFontSizeSetting from './ReaderFontSizeSetting.vue';
+</script>
+
+<template>
+    <div class="flex flex-col bg-background-900 backdrop-blur-md bg-opacity-70 rounded-xl">
+        <div class="flex flex-row w-full px-5 py-5 justify-around gap-5">
+            <ReaderColorSetting v-for="colorScheme in colorSchemes" :key="colorScheme.id" :colorScheme="colorScheme" />
+        </div>
+        <div class="flex flex-row w-full px-5 py-5 justify-around gap-5 bg-background-950 bg-opacity-50">
+            <ReaderFontSetting v-for="font in fonts" :key="font.id" :font="font" />
+        </div>
+        <div class="flex flex-row w-full px-5 py-5 justify-around gap-5">
+            <ReaderFontSizeSetting v-for="fontSize in fontSizes" :key="fontSize.id" :fontSize="fontSize" />
+        </div>
+    </div>
+</template>
