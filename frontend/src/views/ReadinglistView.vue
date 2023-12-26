a115fc9784c510ad872db94c6d5a7eb614ed8ced
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2023-12-26 14:03:09 +0100

    :children_crossing: Remove clear list button when list is empty

--- frontend/src/views/ReadinglistView.vue
+++ frontend/src/views/ReadinglistView.vue
@@ -27,8 +27,8 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
         <div v-if="showClearListModal" @click="showClearListModal = false"
             class="fixed z-40 top-0 bottom-0 left-0 right-0 h-screen w-screen bg-opacity-70 bg-black flex justify-center items-center">
             <div class="w-[90%] max-w-lg bg-background-950 rounded-xl p-5 relative">
-                <X class="absolute top-5 right-5 cursor-pointer" size="32" @click="showClearListModal = false" />
-                <div class="text-xl">Clear List?</div>
+                <X class="absolute top-5 right-5 cursor-pointer" size="26" @click="showClearListModal = false" />
+                <div class="text-xl font-semibold">Clear List?</div>
                 <div class="text-background-600 mb-5">Which articles do you want to remove?</div>
                 <div class="flex flex-col gap-5">
                     <button @click="() => {
@@ -39,7 +39,7 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
                     <button @click="() => {
                         store.clear(true)
                         showClearListModal = false
-                    }" class="w-full h-14 bg-primary-600 rounded-lg transition">Remove only
+                    }" class="w-full h-14 bg-primary-600 rounded-lg transition font-semibold">Remove only
                         read articles</button>
                 </div>
             </div>
@@ -47,12 +47,12 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
         <div class="px-5 overflow-y-scroll pb-10">
             <TitleNavigationBar title="Reading List" backNavigationPath="/" class="mb-5">
                 <div class="flex gap-4">
-                    <ListX size="24" class="text-white cursor-pointer" @click="showClearListModal = true" />
+                    <ListX v-if="store.articles.length > 0" size="24" class="text-white cursor-pointer"
+                        @click="showClearListModal = true" />
                     <RouterLink to="/readinglist/settings" class="text-white">
                         <Settings size="24" class="text-white" />
                     </RouterLink>
                 </div>
-
             </TitleNavigationBar>
             <ReadinglistList />
             <Transition name="fade">
