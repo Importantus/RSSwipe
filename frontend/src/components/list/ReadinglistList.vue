6b96fb54d625ef2d717f934e609a0a847a47f0bb
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-27 18:40:14 +0100

    ✨ Added starred-list as readinglist option

--- frontend/src/components/list/ReadinglistList.vue
+++ frontend/src/components/list/ReadinglistList.vue
@@ -4,6 +4,11 @@ import ReadinglistItem from './ReadinglistItem.vue';
 import { Trash2 } from 'lucide-vue-next';
 import { Star } from 'lucide-vue-next';
 
+defineProps<{
+    isStarred: boolean
+}>();
+
+
 const store = useReadingListStore();
 
 store.update();
@@ -20,7 +25,20 @@ store.update();
         </div>
         <div v-else>
             <TransitionGroup name="list" tag="div" class="flex flex-col gap-3">
-                <ReadinglistItem v-for="article in store.articles" :key="article.articleInfo.id"
+                <ReadinglistItem v-if="isStarred" v-for="article in store.getStarredArticles" :key="article.articleInfo.id"
+                    :article="article.articleInfo" @swipe-right="store.removeArticle(article.articleInfo)"
+                    :downloaded="article.content !== undefined" :swipe-right="{
+                        color: 'red',
+                        name: 'Remove',
+                        removeCard: true,
+                        icon: Trash2
+                    }" :swipe-left="{
+    color: 'orange',
+    removeCard: false,
+    name: 'Star',
+    icon: Star
+}" />
+                <ReadinglistItem v-if="!isStarred" v-for="article in store.articles" :key="article.articleInfo.id"
                     :article="article.articleInfo" @swipe-right="store.removeArticle(article.articleInfo)"
                     :downloaded="article.content !== undefined" :swipe-right="{
                         color: 'red',
