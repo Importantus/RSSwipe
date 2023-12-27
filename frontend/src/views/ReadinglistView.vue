4669a5fcf1fbfaf5642bf3fba8af83625faf6a9a
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2023-12-27 18:40:38 +0100

    ✨ Added starred list button when user has starred articles

--- frontend/src/views/ReadinglistView.vue
+++ frontend/src/views/ReadinglistView.vue
@@ -3,12 +3,12 @@ import TitleNavigationBar from '@/components/TitleNavigationBar.vue';
 import ReadinglistList from '@/components/list/ReadinglistList.vue';
 import { useReadingListStore } from '@/stores/readingList';
 import { Settings } from 'lucide-vue-next';
-import { Undo2 } from 'lucide-vue-next';
+import { Undo2, BookText, Star } from 'lucide-vue-next';
 import { ref, watch } from 'vue';
 
 const store = useReadingListStore();
-
 const showTile = ref(false)
+let typeStarredList = ref(false);
 
 watch(() => store.removedArticles.length, (newValue, oldValue) => {
     if (newValue > oldValue) {
@@ -18,15 +18,29 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
         }, 5000);
     }
 });
+
+function switchListTypeStarred(starList: boolean) {
+    if (starList) {
+        typeStarredList.value = true;
+    } else {
+        typeStarredList.value = false;
+    }
+}
+
+console.log(store.hasStarredArticles)
+
 </script>
 <template>
-    <div class="px-5 overflow-y-scroll pb-10">
+    <div class="px-5 overflow-y-scroll h-screen">
         <TitleNavigationBar title="Reading List" backNavigationPath="/" class="mb-5">
             <RouterLink to="/readinglist/settings" class="text-white">
                 <Settings size="24" class="text-white" />
             </RouterLink>
         </TitleNavigationBar>
-        <ReadinglistList />
+        <div>
+            <ReadinglistList v-if="typeStarredList" :isStarred="true" />
+            <ReadinglistList v-if="!typeStarredList" :isStarred="false" />
+        </div>
         <Transition name="fade">
             <div :onclick="() => {
                 showTile = false
@@ -39,5 +53,27 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
                 <Undo2 size="24" class=" text-primary-600" />
             </div>
         </Transition>
+        <div v-if="store.hasStarredArticles" class="sticky bottom-0 pt-5">
+            <div class="flex flex-col">
+                <div class="flex flex-row justify-between items-center w-full m-auto px-[20%] bg-background-950 p-4">
+                    <button @click=" switchListTypeStarred(false)" class="shrink-0">
+                        <BookText size="24" class=""
+                            :class="{ 'text-primary-600': !typeStarredList, 'opacity-40': typeStarredList }" />
+                    </button>
+                    <button @click="switchListTypeStarred(true)" class="shrink-0">
+                        <Star size="24" class=""
+                            :class="{ 'text-primary-600': typeStarredList, 'opacity-40': !typeStarredList }" />
+                    </button>
+                </div>
+                <div class="flex flex-row">
+                    <div class="h-1 w-1/2 rounded-full"
+                        :class="{ 'bg-primary-600': !typeStarredList, 'bg-background-950': typeStarredList }">
+                    </div>
+                    <div class="h-1 w-1/2 rounded-full"
+                        :class="{ 'bg-background-950': !typeStarredList, 'bg-primary-600': typeStarredList }">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
