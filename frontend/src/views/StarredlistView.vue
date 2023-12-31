c6fa45c418477d81bfe1b14b03142e8ae9532c88
Johann <johann.tessmer@stud.th-luebeck.de>
2023-12-31 03:26:23 +0100

    ✨ Added StarredList

---
+++ frontend/src/views/StarredlistView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import TitleNavigationBar from '@/components/TitleNavigationBar.vue';
+import ReadinglistList from '@/components/list/ReadinglistList.vue';
+import { useReadingListStore } from '@/stores/readingList';
+import { Settings } from 'lucide-vue-next';
+import { Undo2 } from 'lucide-vue-next';
+import { ref, watch } from 'vue';
+
+const store = useReadingListStore();
+const showTile = ref(false)
+
+watch(() => store.removedArticles.length, (newValue, oldValue) => {
+    if (newValue > oldValue) {
+        showTile.value = true
+        setTimeout(() => {
+            showTile.value = false;
+        }, 5000);
+    }
+});
+
+</script>
+<template>
+    <div class="px-5 overflow-y-scroll h-screen">
+        <TitleNavigationBar title="Starred List" backNavigationPath="/readinglist" class="mb-5">
+            <div class="gap-4 flex flex-row">
+                <RouterLink to="/readinglist/settings" class="text-white">
+                    <Settings size="26" class="text-white" />
+                </RouterLink>
+            </div>
+        </TitleNavigationBar>
+        <ReadinglistList :isStarred="true" />
+        <Transition name="fade">
+            <div :onclick="() => {
+                showTile = false
+                store.undo();
+            }" v-if="showTile"
+                class="fixed bottom-5 left-5 right-5 flex flex-row gap-5 bg-black p-5 rounded-md shadow-sm">
+                <div class="flex-grow font-semibold">
+                    Removed Article
+                </div>
+                <Undo2 size="24" class=" text-primary-600" />
+            </div>
+        </Transition>
+    </div>
+</template>
