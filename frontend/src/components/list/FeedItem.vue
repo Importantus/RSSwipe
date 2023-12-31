c3fde4195f33269429da4ae7d48334604b0aa7e2
yamen <mohammad.aljasem@stud.th-luebeck.de>
2023-12-31 18:24:29 +0100

    💄 update the styling

--- frontend/src/components/list/FeedItem.vue
+++ frontend/src/components/list/FeedItem.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import { defineProps,ref} from 'vue';
+import { computed, defineProps,ref} from 'vue';
 import { userFeedItem } from '@/stores/feeds';
 import type { FeedItem } from '@/stores/feeds';
 import { ChevronDown } from 'lucide-vue-next';
-import { ChevronLeft } from 'lucide-vue-next';
-import TitleNavigationBar from '@/components/TitleNavigationBar.vue'
+
 
 
 
@@ -14,29 +13,36 @@ const showOptions = ref(false);
 const props = defineProps<{
     feed: FeedItem;
 }>();
- 
+
 const toggleOptions = () => {
     showOptions.value = !showOptions.value;
 };
-
+const openInApp = computed({
+    get:() => props.feed.openInApp, 
+    set:(value )=>{
+       
+        store.toggleOpenInApp(props.feed.id)
+    }
+}) 
 const shareFeed = () => {
     navigator.share({
         title: props.feed.title,
         url: props.feed.url,
     });
 };
+
 </script>
 
 
 <template>
     
     <div>
-        <div class="flex items-center text-center ">
-            <div class="flex justify-between items-center bg-secondary-800 text-white p-5 rounded-t-lg shadow-md w-full ">
+        <div class="flex items-center text-left ">
+            <div class="flex justify-between  bg-secondary-800/50 text-white p-3 rounded-lg shadow-md w-full  "  :class="{ 'rounded-b-none': showOptions }">
 
                 <img class="w-10 h-10 mr-4" :src="props.feed.faviconUrl" alt="favicon" />
                 <div class="flex-grow mx-5">
-                    <button @click="toggleOptions" class="text-center">
+                    <button @click="toggleOptions" class="text-left">
                         {{ props.feed.title }}
                         <br>
                         <span class=" feed-item ml-2 text-xs text-secondary-500  items-center">(~34 Articles per day)</span> 
@@ -48,22 +54,24 @@ const shareFeed = () => {
             </div>
         </div>
 
-        <div v-if="showOptions" class=" rounded-b-lg bg-secondary-900 ">
+        <div v-if="showOptions" class=" rounded-lg bg-secondary-900/75 "  :class="{ 'rounded-t-none': showOptions }">
             <div class="px-4 py-2">
 
-                <div class="rounded-lg bg-secondary-900 ">
+                <div class="rounded-lg">
                     <div class="px-4 py-2">
                         <h3>Open in App?</h3>
                     </div>
                     <div class="flex justify-center items-center">
                         <div class="px-4 text-secondary-500">if the articles of this feed should be opened in the app or an
                             external tab
+
                         </div>
+                        
                         <div class="px 4">
                             <label class="relative inline-flex items-center me-3 cursor-pointer">
-                                <input type="checkbox" value="" class="sr-only peer" checked>
-                                <div
-                                    class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-500">
+                                <input type="checkbox"  class="sr-only peer" v-model="openInApp">
+                                <div 
+                                    class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 dark:peer-focus:ring-orange-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600">
                                 </div>
 
                             </label>
