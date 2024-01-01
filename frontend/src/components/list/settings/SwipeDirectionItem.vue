61a0c03a15c1da55cfa7b69ccf5607795332b0d3
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-01 19:51:30 +0100

    :lipstick: Highlight selected item with background color

--- frontend/src/components/list/settings/SwipeDirectionItem.vue
+++ frontend/src/components/list/settings/SwipeDirectionItem.vue
@@ -11,9 +11,10 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <div @click="onClick" :class="{ 'border-primary-600 border-2': selected }"
-        class="cursor-pointer w-full p-3 flex flex-col items-center gap-2 justify-center bg-background-900 bg-opacity-30 rounded-lg">
-        <props.ownDirection.icon :style="{ color: props.ownDirection.color }" class="opacity-70" />
+    <div @click="onClick" :class="{ 'bg-background-900 bg-opacity-30': !selected, 'bg-primary-600': selected }"
+        class="cursor-pointer w-full p-3 flex flex-col items-center gap-2 justify-center rounded-lg transition-colors ease-out duration-300">
+        <props.ownDirection.icon :style="{ color: (selected ? 'white' : props.ownDirection.color) }" class="opacity-70"
+            :class="{ 'opacity-100 ': selected }" />
         <span class="text-center">{{ props.ownDirection.name }}</span>
     </div>
 </template>
