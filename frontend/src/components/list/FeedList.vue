d49c7674ee636b50fab7ead38743ecf6f10cc009
yamen <mohammad.aljasem@stud.th-luebeck.de>
2024-01-02 19:53:29 +0100

    💄 improving styling

--- frontend/src/components/list/FeedList.vue
+++ frontend/src/components/list/FeedList.vue
@@ -12,7 +12,7 @@ store.getFeedList();
 
 </script>
 <template>
-    <div class="container  p-4">
+    <div class="container  p-4 overflow-y-scroll">
         <div class="space-y-4">
             <FeedItem v-for="feed in store.feedList" :key="feed.id" :feed="feed"/>
         </div>
