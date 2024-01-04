f9a4b29379fdfcb3ebdc4a5b0975877c9ab9a66b
yamen <mohammad.aljasem@stud.th-luebeck.de>
2024-01-04 17:40:09 +0100

    🎨 improving code format

--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -4,8 +4,7 @@ import { userFeedItem } from '@/stores/feeds';
 import { ref } from 'vue';
 import TitleNavigationBar from '@/components/TitleNavigationBar.vue'
 import TextInputIcon from '@/components/TextInputIcon.vue'
-import { Link, X ,Plus} from 'lucide-vue-next';
-
+import { Link, X, Plus } from 'lucide-vue-next';
 
 const newFeedUrl = ref('');
 const openInApp = ref(true);
@@ -15,7 +14,6 @@ const showModal = ref(false);
 const toggleAddFeedPopup = () => {
   showModal.value = !showModal.value;
 };
-
 const addNewFeed = async () => {
   const shortenedURL = newFeedUrl.value.trim();
   if (shortenedURL) {
@@ -23,68 +21,37 @@ const addNewFeed = async () => {
     await store.addFeed(shortenedURL, openInApp.value);
     newFeedTitle.value = '';
     newFeedUrl.value = '';
-    
   } else {
     alert('Bitte geben Sie eine gültige URL ein.');
-    
   }
-  
 };
 </script>
 
 <template>
   <div>
     <div class="px-5  pb-1 relative z-10">
-    <TitleNavigationBar title="Your Feeds" backNavigationPath="/" />
-  </div>
-  
-    <div >
-      
-      <div class="bg-primary-600 rounded-full  absolute bottom-8 right-0 m-8 py-0 mr-8"> 
-        
-      <Plus :size="24"  @click="showModal = true"
-        class=" text-white m-3" />
+      <TitleNavigationBar title="Your Feeds" backNavigationPath="/" />
+    </div>
+    <div>
+      <div @click="showModal = true" class="bg-primary-600 rounded-full  absolute bottom-8 right-0 m-8 py-0 mr-8">
+        <Plus :size="24"  class=" text-white m-3" />
       </div>
-      
-      
-      <div 
-      v-if="showModal"
-      class="fixed z-40 top-0 bottom-0 left-0 right-0 h-screen w-screen bg-opacity-40 bg-black flex justify-center items-center"
-    >
-    <div class="w-[90%] max-w-lg bg-secondary-900 rounded-xl p-5 relative  justify-between">
-        <X
-        class="absolute mb-3 top-5 right-5 cursor-pointer"
-          size="32"
-          @click="toggleAddFeedPopup" />
-      
-      
-        <h3 class="text-lg font-semibold mb-3 top-5">Add New Feed</h3>
-        <TextInputIcon
-          
-          v-model="newFeedUrl"
-          placeholder="URL"
-          :icon="Link" 
-          :required="true"
-        />
-        
-        
-          
-          
-          <button
-          @click="addNewFeed"
-          class="w-full h-14 bg-amber-600 rounded-lg hover:bg-amber-700 transition  mb-2 mt-4 "
-        >
-        Add Feed
-        </button>
-        
+      <div v-if="showModal"
+        class="fixed z-40 top-0 bottom-0 left-0 right-0 h-screen w-screen bg-opacity-40 bg-black flex justify-center items-center">
+        <div class="w-[90%] max-w-lg bg-secondary-900 rounded-xl p-5 relative  justify-between">
+          <X class="absolute mb-3 top-5 right-5 cursor-pointer" size="32" @click="toggleAddFeedPopup" />
+          <h3 class="text-lg font-semibold mb-3 top-5">Add New Feed</h3>
+          <TextInputIcon v-model="newFeedUrl" placeholder="URL" :icon="Link" :required="true" />
+          <button @click="addNewFeed"
+            class="w-full h-14 bg-amber-600 rounded-lg hover:bg-amber-700 transition  mb-2 mt-4 ">
+            Add Feed
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-    </div>
-   
 
     <FeedList />
-  
+  </div>
 </template>
 
 <style scoped>
