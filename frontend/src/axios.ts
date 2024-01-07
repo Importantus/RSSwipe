a97d5c7bfe6a332b4a1316fc9315cdbd3093e7d8
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-07 21:55:52 +0100

    :children_crossing: When the server returns a 401 error, logout the user

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useAuthStore } from './stores/auth';
+import router from './router';
 
 const instance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL + '/v1'
@@ -17,12 +18,19 @@ instance.interceptors.request.use((config) => {
     return Promise.reject(error);
 })
 
-//Go to 404 page if 404 error
 instance.interceptors.response.use((response) => {
     return response;
 }, (error) => {
+    // Go to 404 page if 404 error
     if (error.response.status === 404) {
-        window.location.href = '/404'
+        router.push('/404')
+    }
+
+    // When the token is expired or invalid, logout the user
+    if (error.response.status === 401) {
+        const authStore = useAuthStore()
+        authStore.logout()
+        router.push('/login')
     }
 
     return Promise.reject(error);
