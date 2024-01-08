2fe066d5a95445611eaef0e308f0fb4884493148
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2024-01-08 23:10:33 +0100

    ✨ Added sequential case for Next Articles

--- frontend/src/stores/reader.ts
+++ frontend/src/stores/reader.ts
@@ -148,7 +148,11 @@ export const useReaderStore = defineStore({
                     this.startIndex = readingListStore.articles.findIndex(a => a.articleInfo.id === article.id)
                     this.storedArticles = readingListStore.articles.slice(this.startIndex, this.startIndex + 1)
                     this.markArticleAsRead(article)
-                    this.getNextUnreadArticle()
+                    if (readingListStore.nextArticleSkipRead) {
+                        this.getNextUnreadArticle()
+                    } else {
+                        this.getNextArticle()
+                    }
                     router.push(`/article/${article.id}`)
                     this.status = ReaderStatus.READY
                     break;
@@ -192,7 +196,11 @@ export const useReaderStore = defineStore({
             this.markArticleAsRead(this.storedArticles[0].articleInfo)
             const readingListStore = useReadingListStore();
             this.startIndex = readingListStore.articles.findIndex(a => a.articleInfo.id === this.storedArticles[0].articleInfo.id)
-            this.getNextUnreadArticle()
+            if (readingListStore.nextArticleSkipRead) {
+                this.getNextUnreadArticle()
+            } else {
+                this.getNextArticle()
+            }
             router.push(`/article/${this.storedArticles[0].articleInfo.id}`)
         },
 
@@ -209,6 +217,16 @@ export const useReaderStore = defineStore({
                 if (unreadArticles.length > 0) {
                     this.storedArticles.push(unreadArticles[0])
                 }
+            }
+            return
+        },
+
+        async getNextArticle() {
+            const readingListStore = useReadingListStore();
+            const remainingArticles = readingListStore.articles.slice(this.startIndex + 1, readingListStore.articles.length)
+            if (remainingArticles.length > 0) {
+                //Unread Articles below the current one
+                this.storedArticles.push(remainingArticles[0])
             }
             return
         },
