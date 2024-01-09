3aadc2734a412f2ea1090044e941a4d64bb684cc
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-09 19:29:52 +0100

    :sparkles: Show url of the feed, improve buttons and add one to open the media closes #102

--- frontend/src/components/list/FeedItem.vue
+++ frontend/src/components/list/FeedItem.vue
@@ -3,6 +3,7 @@ import { computed, defineProps, ref, onMounted } from 'vue';
 import { useFeedStore } from '@/stores/feeds';
 import type { FeedItem } from '@/stores/feeds';
 import { ChevronDown } from 'lucide-vue-next';
+import { Trash2 } from 'lucide-vue-next';
 
 const store = useFeedStore();
 const showOptions = ref(false);
@@ -39,57 +40,69 @@ onMounted(async () => {
         canShare.value = false;
     }
 });
+
+const templateArr: string[] = [
+    "/images/articles/placeholder01.png",
+    "/images/articles/placeholder02.png",
+    "/images/articles/placeholder03.png",
+    "/images/articles/placeholder04.png"
+]
+
+const faviconUrl = props.feed.faviconUrl ? props.feed.faviconUrl : templateArr[Math.round((templateArr.length - 1) * Math.random())]
+
+const openFeedPage = () => {
+    const baseUrl = new URL(props.feed.url).origin
+    window.open(baseUrl, '_blank');
+}
 </script>
 
 <template>
-    <div>
-        <div @click="toggleOptions" class="flex items-center text-left cursor-pointer">
-            <div class="flex justify-between  bg-secondary-800/50 text-white p-4 rounded-lg shadow-md w-full px-4 "
-                :class="{ 'rounded-b-none': showOptions }">
-                <img v-if="props.feed.faviconUrl" class="w-10 h-10 me-auto" :src="props.feed.faviconUrl" alt="favicon" />
-                <div class="flex-grow flex justify-center items-center mx-2">
-                    <button class=" font-semibold ">
-                        {{ props.feed.title }}
-                    </button>
+    <div class="overflow-hidden rounded-lg">
+        <div @click="toggleOptions" class="flex items-center text-left cursor-pointer relative z-10">
+            <div
+                class="flex justify-between bg-[#222] text-white px-4 py-3 rounded-t-lg shadow-md w-full transition items-center gap-3">
+
+                <div class="w-10 h-10 flex-shrink-0 overflow-hidden rounded-sm">
+                    <img class="min-w-full min-h-full" :src="faviconUrl" alt="favicon" />
                 </div>
-                <div class="flex items-center  me-auto ">
-                    <ChevronDown :stroke-width="1.25" size="35" class="{ active: isActive } rounded"
-                        :class="{ 'rotate-180': showOptions }" />
+                <div class="flex flex-col w-full overflow-hidden">
+                    <div class="text-lg font-semibold truncate block max-w-full">
+                        {{ props.feed.title }}
+                    </div>
+                    <div class="truncate block max-w-full text-background-500 text-sm">{{ props.feed.url }}</div>
+                </div>
+                <div class="flex items-center flex-shrink-0 aspect-square">
+                    <ChevronDown :stroke-width="1.25" size="35" class="transition" :class="{ 'rotate-180': showOptions }" />
                 </div>
             </div>
         </div>
         <Transition name="slide-fade">
-            <div v-if="showOptions" class=" rounded-lg bg-secondary-900/75 " :class="{ 'rounded-t-none': showOptions }">
-                <div class="px-4 py-2">
-                    <div class="rounded-lg">
-                        <div class="px-1 py-2">
-                            <h3>Open in App?</h3>
-                        </div>
-                        <div class="flex  justify-between  ">
-                            <div class="px-1 text-secondary-500 text-left">Should Feed Articles Open In App?
-                            </div>
-                            <div class=" px-auto py-1">
-                                <label class="relative inline-flex items-center me-1 cursor-pointer">
-                                    <input type="checkbox" class="sr-only peer" v-model="openInApp">
-                                    <div class="w-11 h-6 bg-primary-500 rounded-full peer dark:bg-background-900  dark:peer-focus:ring-orange-00 peer-checked:after:translate-x-full 
-                                        rtl:peer-checked:after:-translate-x-full peer-checked:after:border-primary-500 after:content-[''] after:absolute after:top-0.5 
-                                        after:start-[2px] after:bg-primary-500 after:border-primary-500 after:border after:rounded-full after:h-5 after:w-5 after:transition-all
-                                         dark:border-background-900  peer-checked:bg-primary-700">
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="flex justify-between py-3 px-1  space-x-2 ">
-                            <div v-if="canShare" class="flex-1">
-                                <button @click="shareFeed();"
-                                    class=" w-full h-11 border-amber-600 border-solid border-2 rounded-lg hover:bg-amber-700 transition py-2 px-20 mr-1 ">Share</button>
-                            </div>
-                            <div class="flex-1">
-                                <button @click="store.deleteFeed(props.feed.id)"
-                                    class="w-full h-11 bg-red-500 rounded-lg hover:bg-red-600 transition py-2 px-20">Delete</button>
-                            </div>
-                        </div>
+            <div v-if="showOptions" class="px-4 py-4 rounded-lg bg-[#282828] flex flex-col gap-4 z-0"
+                :class="{ 'rounded-t-none': showOptions }">
+                <div class="flex justify-between items-center gap-2">
+                    <div>
+                        <h3 class="font-semibold">Open in App?</h3>
+                        <p class="text-secondary-500 text-left font-thin text-sm">Should Feed Articles Open In App?
+                        </p>
                     </div>
+                    <label class="relative inline-flex items-center cursor-pointer">
+                        <input type="checkbox" class="sr-only peer" v-model="openInApp">
+                        <div class="w-11 h-6 bg-primary-600 rounded-full peer dark:bg-background-900  dark:peer-focus:ring-orange-00 peer-checked:after:translate-x-full 
+                                        rtl:peer-checked:after:-translate-x-full peer-checked:after:border-primary-600 after:content-[''] after:absolute after:top-0.5 
+                                        after:start-[2px] after:bg-primary-600 after:border-primary-600 after:border after:rounded-full after:h-5 after:w-5 after:transition-all
+                                         dark:border-background-900  peer-checked:bg-primary-800">
+                        </div>
+                    </label>
+                </div>
+                <div class="flex gap-3">
+                    <button v-if="canShare" @click="shareFeed();"
+                        class=" w-full h-11 bg-primary-600 rounded-lg hover:bg-amber-700 transition">Share</button>
+                    <button @click="openFeedPage();"
+                        class=" w-full h-11 bg-primary-600 rounded-lg hover:bg-amber-700 transition">Open</button>
+                    <button @click="store.deleteFeed(props.feed.id)"
+                        class="w-11 h-11 flex-shrink-0 bg-black/30 rounded-lg hover:bg-red-900 transition flex justify-center items-center">
+                        <Trash2 size="20" class="text-background-300" />
+                    </button>
                 </div>
             </div>
         </Transition>
