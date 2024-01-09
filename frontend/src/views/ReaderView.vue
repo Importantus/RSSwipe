c8dd1a9c7d28169d6e8f79ab86fe86a499207129
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-09 16:12:14 +0100

    :lipstick: Small style fixes

--- frontend/src/views/ReaderView.vue
+++ frontend/src/views/ReaderView.vue
@@ -116,7 +116,8 @@ function calculateUIHide() {
         <div class="fixed top-0 right-0 left-0 m-auto w-full max-w-xl">
             <Transition name="readerbar-hide">
                 <div v-if="!hideUi"
-                    class="flex flex-row items-center justify-between py-5 px-5 z-10 bg-background-950 backdrop-blur-md bg-opacity-70">
+                    class="flex flex-row items-center justify-between px-5 z-10 bg-background-950 backdrop-blur-md bg-opacity-70 transition-all ease-out duration-300"
+                    :class="{ 'py-5': scrollpercent < 1, 'py-2': scrollpercent >= 1 }">
                     <div class="flex flex-row items-center w-[80%]">
                         <router-link :to="backNavigationPath">
                             <MoveLeft size="24" class=" text-inherit" />
@@ -140,9 +141,14 @@ function calculateUIHide() {
                 'border-black text-black bg-white': store.settings.colorScheme.id === colorSchemes.light.id,
                 'border-white text-white bg-background-950': store.settings.colorScheme.id === colorSchemes.dark.id,
             }">
-            <div v-if="store.storedArticles[0] && store.storedArticles[0].content" class="border-inherit">
+            <div v-if="store.storedArticles[0] && store.storedArticles[0].content && store.storedArticles[0].articleInfo"
+                class="border-inherit">
                 <div class="pt-5 border-b-2 border-inherit">
-                    <img :src="store.storedArticles[0].articleInfo.imageUrl" class="pb-5 rounded-md">
+                    <div v-if="store.storedArticles[0].articleInfo.imageUrl"
+                        class="w-full aspect-video bg-background-900 mb-5 rounded-md overflow-hidden">
+                        <img :src="store.storedArticles[0].articleInfo.imageUrl"
+                            class="min-h-full min-w-full after:content-none before:content-none">
+                    </div>
                     <ArticleSource class="pt-2 text-white" :article="store.storedArticles[0].articleInfo" />
                     <div class="text-3xl font-semibold text-inherit py-5"
                         :class="{ 'font-title': store.settings.font.id === fonts.serif.id }">{{
@@ -158,10 +164,10 @@ function calculateUIHide() {
                         }" v-html="DOMPurify.sanitize(store.storedArticles[0].content?.content.content ?? '')">
                 </div>
                 <div v-if="store.storedArticles.length > 1" @click="nextArticle" class="border-inherit">
-                    <div class="border-b-2 border-inherit w-full mb-5"></div>
+                    <div class="border-b-2 border-inherit w-full my-5"></div>
                     <h1 class="text-lg font-bold text-inherit my-2">Next Article:</h1>
-                    <div :style="{ backgroundImage: 'url(' + url + ')' }" class="bg-cover bg-blend-overlay p-5 rounded-2xl"
-                        :class="{
+                    <div :style="{ backgroundImage: 'url(' + url + ')' }"
+                        class="cursor-pointer bg-cover bg-blend-overlay p-5 rounded-2xl" :class="{
                             'bg-[#5c4e38] bg-opacity-80': store.settings.colorScheme.id === colorSchemes.sepia.id,
                             'bg-black bg-opacity-20': store.settings.colorScheme.id === colorSchemes.light.id,
                             'bg-background-950 bg-opacity-75': store.settings.colorScheme.id === colorSchemes.dark.id,
