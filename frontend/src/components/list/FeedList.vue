0743595dfae4c40c759f0ca93677650dbc9d7ffc
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-09 19:23:21 +0100

    :dizzy: Improved fold-out animation closes #99

--- frontend/src/components/list/FeedList.vue
+++ frontend/src/components/list/FeedList.vue
@@ -9,7 +9,9 @@ store.getFeedList();
 </script>
 <template>
             <NonBlockingLoadingIndicator :show="store.state === StoreStatus.LOADING" />
+            <TransitionGroup name="list" tag="div" class="flex flex-col gap-4 overflow-x-hidden">
             <FeedItem v-for="feed in store.feedList" :key="feed.id" :feed="feed" />
+            </TransitionGroup>
         <div v-else class="flex h-3/5 justify-center items-center">
             <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary-600"></div>
         </div>
