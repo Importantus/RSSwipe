f539957f93d0505e6dfc8dc7fcad275fdbbd0f7b
Johann Teßmer <johann.tessmer@stud.th-luebeck.de>
2024-01-10 02:33:01 +0100

    💄 Removed styling from Starred List

--- frontend/src/views/StarredlistView.vue
+++ frontend/src/views/StarredlistView.vue
@@ -3,11 +3,15 @@ import TitleNavigationBar from '@/components/TitleNavigationBar.vue';
 import ReadinglistList from '@/components/list/ReadinglistList.vue';
 import { useReadingListStore } from '@/stores/readingList';
 import { Settings } from 'lucide-vue-next';
+import { ListX, X, Star } from 'lucide-vue-next';
 import { Undo2 } from 'lucide-vue-next';
 import { ref, watch } from 'vue';
 
 const store = useReadingListStore();
+
 const showTile = ref(false)
+
+const showClearListModal = ref(false);
 
 watch(() => store.removedArticles.length, (newValue, oldValue) => {
     if (newValue > oldValue) {
@@ -17,29 +21,45 @@ watch(() => store.removedArticles.length, (newValue, oldValue) => {
         }, 5000);
     }
 });
-
 </script>
 <template>
-    <div class="px-5 overflow-y-scroll h-screen">
-        <TitleNavigationBar title="Starred List" backNavigationPath="/readinglist" class="mb-5">
-            <div class="gap-4 flex flex-row">
-                <RouterLink to="/readinglist/settings" class="text-white">
-                    <Settings size="26" class="text-white" />
-                </RouterLink>
-            </div>
-        </TitleNavigationBar>
-        <ReadinglistList :isStarred="true" />
-        <Transition name="fade">
-            <div :onclick="() => {
-                showTile = false
-                store.undo();
-            }" v-if="showTile"
-                class="fixed bottom-5 left-5 right-5 flex flex-row gap-5 bg-black p-5 rounded-md shadow-sm">
-                <div class="flex-grow font-semibold">
-                    Removed Article
+    <div>
+        <div v-if="showClearListModal" @click="showClearListModal = false"
+            class="fixed z-40 top-0 bottom-0 left-0 right-0 h-screen w-screen bg-opacity-70 bg-black flex justify-center items-center">
+            <div class="w-[90%] max-w-lg bg-background-950 rounded-xl p-5 relative">
+                <X class="absolute top-5 right-5 cursor-pointer" size="26" @click="showClearListModal = false" />
+                <div class="text-xl font-semibold">Clear List?</div>
+                <div class="text-background-400 mb-5">Which articles do you want to remove?</div>
+                <div class="flex flex-col gap-5">
+                    <button @click="() => {
+                        store.clear(false)
+                        showClearListModal = false
+                    }" class="w-full h-14 bg-primary-950 rounded-lg transition font-semibold">Remove all
+                        articles</button>
+                    <button @click="() => {
+                        store.clear(true)
+                        showClearListModal = false
+                    }" class="w-full h-14 bg-primary-600 rounded-lg transition font-semibold">Remove only
+                        read articles</button>
                 </div>
-                <Undo2 size="24" class=" text-primary-600" />
             </div>
-        </Transition>
+        </div>
+        <div class="px-5 overflow-y-scroll pb-10 h-full">
+            <TitleNavigationBar title="Starred List" backNavigationPath="/readinglist" class="mb-5">
+            </TitleNavigationBar>
+            <ReadinglistList :starred="true" />
+            <Transition name="fade">
+                <div :onclick="() => {
+                    showTile = false
+                    store.undo();
+                }" v-if="showTile"
+                    class="fixed bottom-5 left-5 right-5 flex flex-row gap-5 bg-black p-5 rounded-md shadow-sm">
+                    <div class="flex-grow font-semibold">
+                        Removed Article
+                    </div>
+                    <Undo2 size="24" class=" text-primary-600" />
+                </div>
+            </Transition>
+        </div>
     </div>
 </template>
