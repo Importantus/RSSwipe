5cfbfd58af4430b1b12fed80efdd3a4c9b35af23
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-11 18:43:24 +0100

    :ambulance: Do not delete meta tags while sanitizing

--- backend/src/helper/htmlParsing.ts
+++ backend/src/helper/htmlParsing.ts
@@ -22,6 +22,14 @@ export async function getDomFromUrl(url: string): Promise<JSDOM> {
  */
 function sanitizeHtml(html: string) {
     // Sanitize html because of weird jsdom bug (https://github.com/jsdom/jsdom/issues/2177)
-    html = DOMPurify.sanitize(html, { FORBID_TAGS: ['style'] });
+    html = DOMPurify.sanitize(html, {
+        ADD_ATTR: ['content', 'property', 'itemprop', 'datetime'],
+        ADD_TAGS: ['meta', 'head', 'link', 'time'],
+        ADD_URI_SAFE_ATTR: ['property'],
+        FORBID_TAGS: ['style'],
+        ALLOW_DATA_ATTR: true,
+        ALLOW_ARIA_ATTR: true,
+        WHOLE_DOCUMENT: true,
+    });
     return html;
 }
