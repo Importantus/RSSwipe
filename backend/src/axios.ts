a1c069f593ad91b342af3b5ffd0af683e94ceb6f
Soeren Fischer <soeren.fischer@stud.th-luebeck.de>
2024-01-20 00:37:49 +0100

    :alien: Add user agent to prevent 403 errors

--- backend/src/axios.ts
+++ backend/src/axios.ts
@@ -4,6 +4,7 @@ const instance = axios.create();
 
 instance.interceptors.request.use((config) => {
     config.headers.Cookie = "DSGVO_ZUSAGE_V1=true; cookieconsent_status=allow"
+    config.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0"
     return config
 }, (error) => {
     return Promise.reject(error);
