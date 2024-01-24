78e07798649f462b51288faceb9f13e2a5e78007
Malte Fischer <malte.fischer@stud.th-luebeck.de>
2024-01-25 00:02:05 +0100

    :sparkles: Display notification on home view if a feed has errors

--- frontend/src/components/home/navigation/HomeFeedsButton.vue
+++ frontend/src/components/home/navigation/HomeFeedsButton.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import router from '@/router';
+import { useFeedStore } from '@/stores/feeds';
 import { Rss } from 'lucide-vue-next';
+import { ref } from 'vue';
 
 function openFeedSettings() {
     router.push({ name: 'Feeds' });
 }
+const store = useFeedStore();
+
+let someFeedHasError = ref<boolean>(store.someFeedHasError());
 </script>
 
 <template>
-    <button @click="openFeedSettings" title="Your Feed">
+    <button @click="openFeedSettings" title="Your Feed" class="relative error-dot"
+        :class="{ 'error-dot-active': someFeedHasError }">
         <Rss size="24" class=" text-white" />
     </button>
 </template>
