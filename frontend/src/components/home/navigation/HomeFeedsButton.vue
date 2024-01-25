dae5d82750f013e16443261615c0e0fe440090ad
Malte Fischer <malte.fischer@stud.th-luebeck.de>
2024-01-25 17:52:00 +0100

    :children_crossing: Make the notification dot on the home view reactive

--- frontend/src/components/home/navigation/HomeFeedsButton.vue
+++ frontend/src/components/home/navigation/HomeFeedsButton.vue
@@ -2,19 +2,17 @@
 import router from '@/router';
 import { useFeedStore } from '@/stores/feeds';
 import { Rss } from 'lucide-vue-next';
-import { ref } from 'vue';
 
 function openFeedSettings() {
     router.push({ name: 'Feeds' });
 }
 const store = useFeedStore();
 
-let someFeedHasError = ref<boolean>(store.someFeedHasError());
 </script>
 
 <template>
     <button @click="openFeedSettings" title="Your Feed" class="relative error-dot"
-        :class="{ 'error-dot-active': someFeedHasError }">
+        :class="{ 'error-dot-active': store.someFeedHasError() }">
         <Rss size="24" class=" text-white" />
     </button>
 </template>
