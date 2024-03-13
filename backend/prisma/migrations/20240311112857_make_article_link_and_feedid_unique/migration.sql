13157c9e418370532495ecbe0cecc8b89f8687f8
Draculente <48030656+Draculente@users.noreply.github.com>
2024-03-13 19:36:06 +0100

    Revert unique index (#39)
    * Revert ":card_file_box: Add unique constraint to article"
    
    This reverts commit 8612514df41aa0979b2ebd67ff038f33c4d5b87c.
    
    * Revert ":zap: Use findunique to query the index"
    
    This reverts commit e79993a1b85c08c4f3f77864ae0a01a754c4fe54.

--- backend/prisma/migrations/20240311112857_make_article_link_and_feedid_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[link,feedId]` on the table `Article` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Article_link_feedId_key` ON `Article`(`link`, `feedId`);
